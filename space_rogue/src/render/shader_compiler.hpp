fbd2b2f122023453e48246b36c59cd643866fff1
Jobsbka <zorguskimp@gmail.com>
2026-04-07 18:35:10 +0300

    0

--- space_rogue/src/render/shader_compiler.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vulkan/vulkan.h>
-#include <vector>
-#include <string>
-
-std::vector<char> readFile(const std::string& filename);
-VkShaderModule createShaderModule(VkDevice device, const std::vector<char>& code);
